f475fae44c6a5a9db6201c0c6a708e8f88033f35
Leo Škarpa <leo.skarpa@gmail.com>
2023-05-30 15:01:24 +0200

    Axios fixes

--- IOT-client/src/hooks/useAxios.tsx
+++ IOT-client/src/hooks/useAxios.tsx
@@ -1,19 +1,30 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosError, AxiosInstance } from 'axios';
 import { useContext } from 'react';
-import { UserContext } from '../context/userContext'
+import { UserContext } from '../context/userContext';
 
 const useAxios = (): AxiosInstance => {
-  const { user } = useContext(UserContext)!;
+  const { user, setUser } = useContext(UserContext)!;
 
   const instance = axios.create({
     baseURL: 'http://161.53.19.19:45080/',
   });
 
- 
-  instance.interceptors.request.use((config) => {
+  instance.interceptors.request.use(config => {
     config.headers['X-Authorization'] = `Bearer ${user.token}`;
     return config;
   });
+
+  instance.interceptors.response.use(
+    response => response,
+    (error: AxiosError) => {
+      if (error.response?.status === 401) {
+        setUser({ token: '', refreshToken: '', name: '', customerId: '' });
+        localStorage.removeItem('user');
+      }
+
+      return Promise.reject(error);
+    }
+  );
 
   return instance;
 };
